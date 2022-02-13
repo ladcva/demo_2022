1c45e2d0ac040fbb9776af8e7a11bc2a8d4718f7
Anh Duc Le <ladcva@yahoo.com>
2022-02-13 23:41:04 +0700

    Replaced print function

--- a.py
+++ a.py
@@ -1,16 +1,10 @@
 import requests
-import json
-import sys
 
 # create a http request to api.github.com
 url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
 
 def print_response(response):
-    # print the response
-    # print(response.url)
-    # print(response.status_code)
-    # print(response.headers)
-    # print(response.text)
+    print(f"Status code: {response.status_code}")
 
     # to text file
     with open('response.txt', 'w') as f:
